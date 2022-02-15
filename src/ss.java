dd187eda7f73bf8cd3d200c247984c4fabc41f88
kristaapsm <kristaps.muurnieks@gmail.com>
2022-02-15 11:06:03 +0200

    ddd

---
+++ src/ss.java
@@ -0,0 +1,21 @@
+public class ss {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 25;
+        int c = 1200;
+        long d = 5000000;
+        float e = 123.4f;
+        double f = 1.23123;
+        char g = 'g';
+        boolean h = false;
+
+        System.out.println("This is byte data type "+a);
+        System.out.println("This is short data type "+b);
+        System.out.println("This is int data type "+c);
+        System.out.println("This is long data type "+d);
+        System.out.println("This is float data type "+e);
+        System.out.println("This is double data type "+f);
+        System.out.println("This is char data type "+g);
+        System.out.println("This is boolean data type "+h);
+    }
+}
